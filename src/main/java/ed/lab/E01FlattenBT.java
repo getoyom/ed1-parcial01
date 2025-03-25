6ce2cde810b11966dbdc56e15da09c04d23b777f
getoyom <getoyom@correo.url.edu.gt>
2025-03-25 08:35:12 -0600

    Parcial

--- src/main/java/ed/lab/E01FlattenBT.java
+++ src/main/java/ed/lab/E01FlattenBT.java
@@ -1,9 +1,60 @@
 package ed.lab;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
 public class E01FlattenBT {
 
     public void flatten(TreeNode<Integer> root) {
+        if (root == null) {
+            return ;
+        }
+        Stack<TreeNode<Integer>> stack = new Stack<>();
+        TreeNode<Integer> current = root;
+        if(root.right != null) {
+            stack.push(root.right);
 
+        }
+        if(root.left != null) {
+            stack.push(root.left);
+
+        }
+        while (!stack.isEmpty()) {
+            TreeNode<Integer> curr = stack.pop();
+            current.right = curr;
+
+            if (curr.right != null) {
+                stack.push(curr.right);
+            }
+            if (curr.left != null) {
+                stack.push(curr.left);
+            }
+            current.left = null;
+            current = current.right;
+        }
     }
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
